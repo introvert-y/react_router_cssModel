9bec6080301204920d22e96322ecdac2e1bb12d6
mac company <m15577822957@163.com>
2019-09-21 15:05:55 +0800

    module添加

---
+++ src/page/module/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class Page extends React.Component {
+  constructor(props) {
+    this.state = {
+    }
+  }
+  render() {
+    return;
+  }
+}
+
+export default Page;
