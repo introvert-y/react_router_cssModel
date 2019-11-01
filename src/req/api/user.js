45d096d70dada07eac0c400cea4933fe2f4a2601
mac company <m15577822957@163.com>
2019-11-01 20:52:53 +0800

    req封装

--- src/req/api/user.js
+++ src/req/api/user.js
@@ -2,9 +2,6 @@ const api = {
   getMyInfo: {
     url: '/eobi-api/api/sys/currentUserInfo',
   },
-  getMyAuthority: {
-    url: '/eobi-api/api/sys/getCurrentPermissions',
-  },
 };
 
 export default api;
