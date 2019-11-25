85b6ffb1acf2714ddd48dd2630e1e350b780e568
mac company <m15577822957@163.com>
2019-11-25 11:57:39 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 // 如何实现一个react-router路由拦截（导航守卫） https://juejin.im/post/5c31aede6fb9a04a0d570107
-import React from 'react';
-import { BrowserRouter, HashRouter, Route, Link,Switch } from 'react-router-dom';
+import React from "react";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
 // import { browserHistory, Router} from 'react-router';
-import {
-  Modal, message,
-} from 'antd';
-import Home from '../page/CssTest/index';
-import history from '../history';
-import Counter from '../page/Counter/index';
-import Optimize from '../page/Optimize/index';
-import EchartBox from '../page/echartBox/index';
-import ChinaMap from '../page/chinaMap/index';
-import UseMemo from '../page/useMemo/index';
-import UseCallback from '../page/useCallback/index';
-import Memo from '../page/memoParent/index';
-import HeightEchart from '../page/heightEchart/index';
-import FormBox from '../page/form/index';
-import PromiseTest from '../page/promise/index';
+import { Modal, message } from "antd";
+import PropTypes from "prop-types";
+import Home from "../page/CssTest/index";
+import history from "../history";
+import Counter from "../page/Counter/index";
+import Optimize from "../page/Optimize/index";
+import EchartBox from "../page/echartBox/index";
+import ChinaMap from "../page/chinaMap/index";
+import UseMemo from "../page/useMemo/index";
+import UseCallback from "../page/useCallback/index";
+import Memo from "../page/memoParent/index";
+import HeightEchart from "../page/heightEchart/index";
+import FormBox from "../page/form/index";
+import PromiseTest from "../page/promise/index";
 
 /**
  * 解决ant-design的路由跳转，modal和message组件不关闭问题
@@ -35,7 +34,7 @@ function Header() {
         echart
         <ul>
           <li>
-          <Link to="/echart">echart的简单应用</Link>
+            <Link to="/echart">echart的简单应用</Link>
           </li>
           <li>
             <Link to="/map">echart实现中国地图并实现图层叠加</Link>
@@ -49,31 +48,57 @@ function Header() {
         react-router(可打开控制台观察数据的变化)
         <ul>
           <li>
-            <Link to={{ pathname:"/query", query: { name: 'Anny', time: '2', obj: JSON.stringify({
-              name: 'IG',
-              sex: 'man',
-            })}}}>路由query传参</Link>
+            <Link
+              to={{
+                pathname: "/query",
+                query: {
+                  name: "Anny",
+                  time: "2",
+                  obj: JSON.stringify({
+                    name: "IG",
+                    sex: "man"
+                  })
+                }
+              }}
+            >
+              路由query传参
+            </Link>
           </li>
           <li>
-            <Link to={`/link?data=${encodeURIComponent(JSON.stringify({
-              name: 'IG',
-              sex: 'man',
-            }))}`}>
+            <Link
+              to={`/link?data=${encodeURIComponent(
+                JSON.stringify({
+                  name: "IG",
+                  sex: "man"
+                })
+              )}`}
+            >
               路由Link to传一个对象，并url加密
             </Link>
           </li>
           <li>
-            <Link to={{ pathname:"/params" , params: { name: 'Ming', year:'18', obj: JSON.stringify({
-              name: 'IG',
-              sex: 'man',
-            })} }}>路由params传参</Link>
+            <Link
+              to={{
+                pathname: "/params",
+                params: {
+                  name: "Ming",
+                  year: "18",
+                  obj: JSON.stringify({
+                    name: "IG",
+                    sex: "man"
+                  })
+                }
+              }}
+            >
+              路由params传参
+            </Link>
           </li>
           <li>
             <Link to="/queryChildren">嵌套路由</Link>
           </li>
         </ul>
       </li>
-      
+
       <li>
         <Link to="/counter">react-redux的应用和异步流</Link>
       </li>
@@ -99,66 +124,39 @@ function Header() {
   );
 }
 
-function App() {
-  return (
-    // basename 为路由前添加前缀
-    // <BrowserRouter basename={'/douyu'}>
-    <HashRouter >
-      <div style={{ marginLeft: 40 }}>
-        <Header />
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/query" component={RouteQuery} />
-            <Route path="/link" component={RouteLink} />
-            <Route path="/params" component={RouteParams} />
-            <Route path="/queryChildren" component={QueryChildren} />
-            <Route path="/counter" component={Counter} />
-            <Route path="/optimize" component={Optimize} />
-            <Route path="/echart" component={EchartBox} />
-            <Route path="/map" component={ChinaMap} />
-            <Route path="/useMemo" component={UseMemo} />
-            <Route path="/useCallback" component={UseCallback} />
-            <Route path="/memo" component={Memo} />
-            <Route path="/heightEchart" component={HeightEchart} />
-            <Route path="/form" component={FormBox} />
-            <Route path="/testPromise" component={PromiseTest} />
-        </Switch>
-      </div>
-    </HashRouter>
-  );
-}
-
 function RouteLink({ location }) {
-  console.log(JSON.parse(new URLSearchParams(location.search).get('data')));
-  if (!location.search ) {
-    return 'No Click';
+  console.log(JSON.parse(new URLSearchParams(location.search).get("data")));
+  if (!location.search) {
+    return "No Click";
   }
-  const data = JSON.parse(new URLSearchParams(location.search).get('data'));
-  return Object.keys(data).map((item) => (
+  const data = JSON.parse(new URLSearchParams(location.search).get("data"));
+  return Object.keys(data).map(item => (
     <div key={item}>
       {item} : {data[item]}
     </div>
-  ))
+  ));
 }
 
 function RouteQuery({ location }) {
-  console.log('query传参', location);
-  return (<h2>About: { location.query ? `name：${location.query.name}, time: ${location.query.time}` : 'No Click' }</h2>);  
+  console.log("query传参", location);
+  return <h2>About: {location.query ? `name：${location.query.name}, time: ${location.query.time}` : "No Click"}</h2>;
 }
 
-function RouteParams({location}) {
-  console.log('params传参', location);
-  return (<h2>试一下: {location.params ? `name为${location.params.name}, year为${location.params.year}` : 'No Click' }</h2>);
+function RouteParams({ location }) {
+  console.log("params传参", location);
+  return (
+    <h2>试一下: {location.params ? `name为${location.params.name}, year为${location.params.year}` : "No Click"}</h2>
+  );
 }
-
 
 function SonQuery({ location }) {
-  console.log('嵌套路由', location);
+  console.log("嵌套路由", location);
   if (location.query) {
     return location.query.name;
-  } else  if (location.params) {
+  }
+  if (location.params) {
     return location.params.name;
-  } 
+  }
   return <h3> No Click</h3>;
 }
 
@@ -168,10 +166,10 @@ function QueryChildren({ match }) {
       <h2>嵌套路由</h2>
       <ul>
         <li>
-          <Link to={{ pathname:`${match.path}/apple` , query: { name: '苹果' } }}>苹果</Link>
+          <Link to={{ pathname: `${match.path}/apple`, query: { name: "苹果" } }}>苹果</Link>
         </li>
         <li>
-          <Link to={{ pathname:`${match.path}/putao`, params: { name: '葡萄' } }}>葡萄</Link>
+          <Link to={{ pathname: `${match.path}/putao`, params: { name: "葡萄" } }}>葡萄</Link>
         </li>
       </ul>
       <Switch>
@@ -182,4 +180,45 @@ function QueryChildren({ match }) {
   );
 }
 
+function App() {
+  return (
+    // basename 为路由前添加前缀
+    // <BrowserRouter basename={'/douyu'}>
+    <HashRouter>
+      <div style={{ marginLeft: 40 }}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/query" component={RouteQuery} />
+          <Route path="/link" component={RouteLink} />
+          <Route path="/params" component={RouteParams} />
+          <Route path="/queryChildren" component={QueryChildren} />
+          <Route path="/counter" component={Counter} />
+          <Route path="/optimize" component={Optimize} />
+          <Route path="/echart" component={EchartBox} />
+          <Route path="/map" component={ChinaMap} />
+          <Route path="/useMemo" component={UseMemo} />
+          <Route path="/useCallback" component={UseCallback} />
+          <Route path="/memo" component={Memo} />
+          <Route path="/heightEchart" component={HeightEchart} />
+          <Route path="/form" component={FormBox} />
+          <Route path="/testPromise" component={PromiseTest} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+}
+
+RouteParams.propTypes = {
+  location: PropTypes.object.isRequired
+};
+SonQuery.propTypes = {
+  location: PropTypes.object.isRequired
+};
+RouteQuery.propTypes = {
+  location: PropTypes.object.isRequired
+};
+QueryChildren.propTypes = {
+  match: PropTypes.object.isRequired
+};
 export default App;
